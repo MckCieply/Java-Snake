72d693f9d284a14427b96c296941b870e798d500
Alex <mwppl123@gmail.com>
2023-04-12 22:32:46 +0200

    Eating apples

--- snake/src/App.java
+++ snake/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         GameFrame frame = new GameFrame();
-        
+
     }
+    // https://www.youtube.com/watch?v=bI6e6qjJ8JQ 4:45
 }
